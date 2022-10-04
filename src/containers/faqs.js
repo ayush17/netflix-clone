2d9657d4006ea7afa64c04898a1012d63559b198
Ayush Tiwari <ayushtiwari@Ayushs-MacBook-Pro.local>
2022-10-04 17:26:14 +0530

    add accordion component

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,17 +1,19 @@
-import faqsData from '../fixtures/faqs.json';
-import {Accordion} from '../components';
+import React from "react";
+import { Accordion } from "../components";
+import faqsData from "../fixtures/faqs";
 
-export function FaqsContainer(){
-    return (
-        <Accordion>
-            <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-            {faqsData.map((item)=>{
-                return <Accordion.Item key={item.id}>
-                    <Accordion.Header>{item.header}</Accordion.Header>
-                    <Accordion.Body>{item.body}</Accordion.Body>
-                </Accordion.Item>
-            })}
-            <Accordion.Item />
-        </Accordion>
-    )
+export function FaqsContainer() {
+  return (
+    <Accordion>
+      <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+      <Accordion.Frame>
+        {faqsData.map((item) => (
+          <Accordion.Item key={item.id}>
+            <Accordion.Header>{item.header}</Accordion.Header>
+            <Accordion.Body>{item.body}</Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion.Frame>
+    </Accordion>
+  );
 }
