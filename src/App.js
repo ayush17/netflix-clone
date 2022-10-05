49e6ec778f6cdaaf914e54af243d77ee97f54ae0
Ayush Tiwari <ayushtiwari@Ayushs-MacBook-Pro.local>
2022-10-06 00:27:13 +0530

    add Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { FaqsContainer } from "./containers/faqs";
-import { FooterContainer } from "./containers/footer";
-import { JumbotronContainer } from "./containers/jumbotron";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home, Browse, Signin, Signup } from "./pages";
+import * as routes from "./constants/routes";
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Routes>
+        <Route exact path={routes.Home} element={<Home />}></Route>
+        <Route exact path={routes.SIGN_IN} element={<Signin />}></Route>
+        <Route exact path={routes.SIGN_UP} element={<Signup />}></Route>
+        <Route exact path={routes.Browse} element={<Browse />}></Route>
+      </Routes>
+    </Router>
   );
 }
